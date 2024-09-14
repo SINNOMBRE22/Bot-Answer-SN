32878a1969743ad9378de849ccb253b510d1e6ec
Cristian <107384946+SINNOMBRE22@users.noreply.github.com>
2024-09-14 05:36:42 -0600

    Update SN.js

--- SN.js
+++ SN.js
@@ -337,7 +337,8 @@ https://chat.whatsapp.com/EcMClegA2DVBZRiudPqYqP
                     // Aviso de activación una vez que el bot esté listo después del reinicio
                     client.on('ready', () => {
                         message.reply('🚀 El bot se ha activado nuevamente y está listo para usar.');
-                }
+                        });
+                    }
             });
   
                 default:
