6f37169e51ff70896d709c481c4a061fe656cceb
Cristian <107384946+SINNOMBRE22@users.noreply.github.com>
2024-09-14 05:55:29 -0600

    Update SN.js

--- SN.js
+++ SN.js
@@ -307,7 +307,7 @@ https://chat.whatsapp.com/EcMClegA2DVBZRiudPqYqP
      `);
                     break;
     // Verificar si el mensaje es un comando específico (ej: "$actualizar")
-    case 'actualizar') {
+    case 'actualizar':
     message.reply('🔄 Actualizando el bot...');
 
     // Ejecutar el comando git pull para actualizar el bot
